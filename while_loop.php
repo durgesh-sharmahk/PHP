3189d320a6dd88435a187b992826c3a5d92a332d
Durgesh Sharma <59818011+durgesh-sharmahk@users.noreply.github.com>
2020-12-28 13:57:44 +0530

    Add files via upload

---
+++ while_loop.php
@@ -0,0 +1,26 @@
+<?php
+$a=1;
+$b=0;
+$c=0;
+echo "<ul>";
+while($a<=10){
+    echo $a. " . hello", "<br>";
+    
+    $a=$a+2;
+    
+}
+while ($b<=10){
+    echo"durgesh", "<br>"; 
+    $b=$b+2;       
+}
+//by using list tag of html
+while($c<=10){
+    echo "<li>". $c. ") hello", "<br>" , "</li>";
+    
+    $c++;
+    
+}
+
+
+
+?>
