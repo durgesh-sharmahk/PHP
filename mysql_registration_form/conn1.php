17a296b5f4485a55bfd7ea99da8355c33ba09262
Durgesh Sharma <59818011+durgesh-sharmahk@users.noreply.github.com>
2021-01-05 14:00:31 +0530

    Update conn1.php

--- mysql_registration_form/conn1.php
+++ mysql_registration_form/conn1.php
@@ -5,8 +5,11 @@
         $pass=$_POST['pass'];
         $email=$_POST['email'];
         $error = NULL;
+        //regular expressions
         $regx1 = preg_match("/[a-zA-z]/",$name);
         $regx2 = preg_match("/^[a-zA-Z0-9]{1,20}@[a-z]{1,8}\.[a-z]{2,3}$/",$email);
+        //for form validations
+        //1. for validating name
         if($name == "" || strlen($name) >50)
         {
             $GLOBALS['error'] = "Enter your name";
@@ -17,6 +20,7 @@
             $GLOBALS['error'] = "n";
             echo "Please enter valid name <br>";
         }
+        //for validating passwrd
         if($pass == "")
         {
             $GLOBALS['error'] = "Please enter your password";
@@ -27,6 +31,7 @@
             $GLOBALS['error'] = "please enter your pass";
             echo "Please enter password in not less than 8 characters <br>";
         }
+        //for validating email id
         if($email == "" || strlen($email) >50)
         {
             $GLOBALS['error'] = "Enter your email";
@@ -37,6 +42,7 @@
             $GLOBALS['error'] = "t";
             echo "Please enter valid email id <br>";
         }
+        // checking whether any field has errors. and if there are no errors i.e errors==Null then connect database
         if($GLOBALS['error']==NULL)
         {
             $hostname = "localhost";
@@ -44,14 +50,18 @@
             $password = "";
             $dbname = "durgeshdb";
             try{
+                //for database connection
                 $db=new mysqli($hostname, $username, $password, $dbname);
                 echo "Connected"."<br>";
                 if($db->connect_error)
                 {
                     die("connection failed". $db->connect_error);
                 }
+                //query for insertion of data in database
                 $query1= "INSERT INTO `reg1` (`Name`, `Password`, `E-mail`) VALUES ('$name', '$pass', '$email');";
                 $db->query($query1);
+                
+                //query to display data from database
                 $query2= "SELECT * FROM reg1";
                 $result = $db->query($query2);
                 while($reg1 = $result->fetch_assoc())
@@ -70,6 +80,7 @@
                 }
         }
     }
+//to redirect user to the main login page 
     else
     {
         header("location: regis1.php");
